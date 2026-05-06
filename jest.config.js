fd539af8b5953d4f10a66093e63afe6b5cf5c0ad
Khizar-Shhaid <shahidkhizar1403@gmail.com>
2026-05-06 22:51:18 +0500

    Fix Jest ES module syntax error and ignore non-Jest test files

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: 'node',
+  transform: {},
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/frontend/',
+    '<rootDir>/tests/e2e/',
+    '<rootDir>/tests/performance/'
+  ]
+};
